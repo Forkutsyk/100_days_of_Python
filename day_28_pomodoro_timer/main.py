11c104c477bccc3d43bcd7762f641338b95ea228
Forkutsyk <oleksand.poplavskiy@gmail.com>
2024-08-17 05:26:33 +0200

    day 28. adding highlight thickness in tomato image

--- day_28_pomodoro_timer/main.py
+++ day_28_pomodoro_timer/main.py
@@ -77,10 +77,10 @@ window.config(padx=100, pady=50, bg=YELLOW, highlightthickness=0)
 title = Label(window, text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50))
 title.grid(column=1, row=0)
 
-canvas = Canvas(window, width=200, height=224, bg=YELLOW)
+canvas = Canvas(window, width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(102, 112, image=tomato_img)
-timer_text = canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 start_button = Button(window, text="Start", bg="white", highlightthickness=0, command=start_timer)
