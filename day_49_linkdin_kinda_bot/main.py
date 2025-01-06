cb017701f78cd7ca054f06efe36fa132da48373a
Forkutsyk <oleksand.poplavskiy@gmail.com>
2025-01-06 20:42:06 +0100

    Working on day 49. adding the job offer review functionality + scrolling script

--- day_49_linkdin_kinda_bot/main.py
+++ day_49_linkdin_kinda_bot/main.py
@@ -12,6 +12,11 @@ LINKEDIN_PASSWORD = os.getenv("LINK_PASSWORD")
 LINK = os.getenv("APPLICATION_LINK")
 MINIMUM = 5
 
+# I don't want to apply for every "easy apply" job available, want to change a little the ending functionality.
+# also have a problem if I run the bot too often (2 times in 5 min), LinkedIn keep redirecting me to the another screen of logging
+
+# Probably just use this project to practice with selenium XD
+
 
 def get_driver():
     """Function returns the selenium Driver"""
@@ -35,12 +40,37 @@ def automate_login(driver):
     password.send_keys(LINKEDIN_PASSWORD, Keys.ENTER)
 
 
+# Should I keep this 1 line def XD
+def get_all_list_items(driver):
+    """
+    Function to get all elements from the job list.
+    Input: Selenium driver
+    Output: List of WebElement objects
+    """
+
+    return driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
+
+
 def main():
     driver = get_driver()
     driver.get(LINK)
-    print("Starting doing nasty stuff...")
+    print("Starting doing some stuff...")
     time.sleep(2)
     automate_login(driver)
+
+    list_items = get_all_list_items(driver)
+    print(len(list_items))
+    for element in list_items:
+        # don't know if this script is working correctly// need to test it
+        driver.execute_script('arguments[0].scrollIntoView({ behavior: "smooth", block: "start" });', element)
+        time.sleep(2)
+        element.click()
+        time.sleep(2)
+        position = element.find_element(By.CSS_SELECTOR, value=".job-card-list__title--link")
+        company_name = element.find_element(By.CSS_SELECTOR, value=".artdeco-entity-lockup__subtitle")
+        print(f'Position: {position.get_attribute("aria-label")}')
+        print(f'Company: {company_name.text}\n')
+    driver.quit()
 
 
 if __name__ == '__main__':
