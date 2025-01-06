266b525fa5fe6a5b05f684a3590e9b8abc43444b
Forkutsyk <oleksand.poplavskiy@gmail.com>
2025-01-06 17:44:51 +0100

    Working on day 49. rearranging the code into the def. Added some sleep(2) bypass redirect (testing)

--- day_49_linkdin_kinda_bot/main.py
+++ day_49_linkdin_kinda_bot/main.py
@@ -3,26 +3,46 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 load_dotenv("../.env")
 
 LINKEDIN_LOGIN = os.getenv("LINK_LOGIN")
 LINKEDIN_PASSWORD = os.getenv("LINK_PASSWORD")
 LINK = os.getenv("APPLICATION_LINK")
+MINIMUM = 5
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option('detach', True)
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(LINK)
 
-if __name__ == '__main__':
-    sign_up_button = driver.find_element(By.XPATH, '//*[@id="base-contextual-sign-in-modal"]/div/section/div/div/div/div[2]/button')
-    print("Doin stuff...")
+def get_driver():
+    """Function returns the selenium Driver"""
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('detach', True)
+    return webdriver.Chrome(options=chrome_options)
+
+
+def automate_login(driver):
+    """
+    Function automatically logging in the system.
+    Input: Selenium driver
+    Output: None
+    """
+    sign_up_button = driver.find_element(By.XPATH, value='//*[@id="base-contextual-sign-in-modal"]/div/section/div/div/div/div[2]/button')
     sign_up_button.click()
     login = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal_session_key"]')
     password = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal_session_password"]')
+    time.sleep(2)
     login.send_keys(LINKEDIN_LOGIN)
     password.send_keys(LINKEDIN_PASSWORD, Keys.ENTER)
 
-# ## just testing the login functionality
-# still need to working around a "Application functionallity"  i think in my case just the "Save" functionallity
+
+def main():
+    driver = get_driver()
+    driver.get(LINK)
+    print("Starting doing nasty stuff...")
+    time.sleep(2)
+    automate_login(driver)
+
+
+if __name__ == '__main__':
+    main()
+    print("END")
